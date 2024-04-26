85c11935570a3953b56048c525c7f0235fde57fe
natskoe <110753197+natskoe@users.noreply.github.com>
2024-04-25 21:44:33 -0300

    Update script.js

--- ex_js/script.js
+++ ex_js/script.js
@@ -7,7 +7,6 @@ window.onload = function(){
     let controleCor = 0;
 
     let botao = document.getElementById("botao");
-    console.log(botao);
 
     function alterarCor(){
         paragrafo.style.backgroundColor = cores[controleCor];
@@ -24,24 +23,28 @@ window.onload = function(){
     }
     
     let formulario = document.getElementById("formulario");
-    function validarCampo(){
+
+    formulario.addEventListener("submit", validarCampo);
+
+    function validarCampo(event){
         let camposVazios = "";
         for(let i = 0; i<formulario.children.length; i++){
             if(formulario.children[i].value == ""){
-                camposVazio += `${formulario.children[i].name}, `
+                camposVazios += `${formulario.children[i].name}, `
             }
         }
-    }
-    let mensagem = document.getElementById("resultado");
-    if (camposVazios == ""){
-        mensagem.innerHTML = `Formulário enviado!`
-    }else{
-        camposVazios = camposVazios.trim();
-        camposVazios = camposVazios.substring(0,camposVazios.length-1);
-        mensagem.innerHTML = `Os campos ${camposVazios} estão vazios!`;
+        let mensagem = document.getElementById("resultado");
+        if (camposVazios == ""){
+            mensagem.innerHTML = `Formulário enviado!`
+        }else{
+            camposVazios = camposVazios.trim();
+            camposVazios = camposVazios.substring(0,camposVazios.length-1);
+            mensagem.innerHTML = `Os campos ${camposVazios} estão vazios!`;
+            console.log(camposVazios)
+        }
+        event.preventDefault()
     }
 
-    formulario.addEventListener("submit", validarCampo);
     select.addEventListener("change", adicionarParagrafo);
     botao.addEventListener("click",alterarCor);
 }
